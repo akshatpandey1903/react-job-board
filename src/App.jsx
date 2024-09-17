b27c62d8c51d3d4539619f7af62f963e1bc57f22
Akshat Pandey <akshatp1903@gmail.com>
2024-09-17 20:09:56 +0530

    Adding routes to different links

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
-const App = () => {
-  const name = "John";
-  const x = 10;
-  const y = 20;
-  const names = ['Gayatri', 'Akshat', 'Bhavini', 'Varun'];
-  const loggedIn = true;
-  const styles = {
-    color: 'red',
-    fontSize: '24px',
-  }
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom"
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
-  return (
-    <> 
-      <div className="text-5xl">App</div>
-      <p style={styles} >Hello { name }</p>
-      <p>The sum of {x} and {y} is {x + y} </p>
-      <ul>
-        { names.map((name, index) => (
-          <li key={index}>{name}</li>
-        ))}
-      </ul>
-      {loggedIn && <h1>Hello Member</h1>}
-    </>
-  );
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={ <MainLayout /> }>
+      <Route index element={ <HomePage /> } />
+      <Route path='/jobs' element={ <JobsPage /> } />
+      <Route path='*' element={ <NotFoundPage /> } />
+    </Route>
+  )
+);
+
+const App = () => {
+  return <RouterProvider router={router} />
 };
 
 export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import logo from '../assets/images/logo.png';
 
 const Navbar = () => {
+  const LinkClass = ({ isActive }) => isActive ? 'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2' : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2';
+
   return (
     <>
     <nav className="bg-indigo-700 border-b border-indigo-500">
@@ -11,7 +13,7 @@ const Navbar = () => {
             className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
           >
             {/* LOGO */}
-            <Link 
+            <NavLink 
               className="flex flex-shrink-0 items-center mr-4" to="/">
               <img
                 className="h-10 w-auto"
@@ -21,24 +23,24 @@ const Navbar = () => {
               <span className="hidden md:block text-white text-2xl font-bold ml-2"
                 >React Jobs</span
               >
-            </Link>
+            </NavLink>
             <div className="md:ml-auto">
               <div className="flex space-x-2">
-                <Link
+                <NavLink
                   to="/"
-                  className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  className={ LinkClass }
                   >Home
-                  </Link>
-                <Link
+                  </NavLink>
+                <NavLink
                   to="/jobs"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  className={ LinkClass }
                   >Jobs
-                  </Link>
-                <Link
+                  </NavLink>
+                <NavLink
                   to="/add-job"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                  className={ LinkClass }
                   >Add Job
-                  </Link>
+                  </NavLink>
               </div>
             </div>
           </div>
